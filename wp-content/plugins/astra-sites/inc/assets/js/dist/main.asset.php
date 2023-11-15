6332f1e827d52121bb44ab5aabfa3065acc25410
die-lolol <die.lolol.1.0@gmail.com>
2023-11-15 14:29:03 +0100

    your
    defense is terrified

--- wp-content/plugins/astra-sites/inc/assets/js/dist/main.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '9148b100aa4443014ad7');
--- wp-content/themes/astra/admin/assets/build/dashboard-app.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'd0e47985aedc6cb6afd8');
