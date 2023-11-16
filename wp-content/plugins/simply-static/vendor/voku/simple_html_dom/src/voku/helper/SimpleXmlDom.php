ac6d4f17eb6426a022c01b456820c7d7274d550c
die-lolol <die.lolol.1.0@gmail.com>
2023-11-16 11:56:47 +0100

    d
    a

---
+++ wp-content/plugins/simply-static/vendor/voku/simple_html_dom/src/voku/helper/SimpleXmlDom.php
@@ -0,0 +1,24 @@
+<br>
+<font size="1"><table class="xdebug-error xe-uncaught-exception" dir="ltr" border="1" cellspacing="0" cellpadding="1">
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Fatal error: Uncaught Error: Class "voku\helper\AbstractSimpleXmlDom" not found in C:\wamp64\www\Velotech\wp-content\plugins\simply-static\vendor\voku\simple_html_dom\src\voku\helper\SimpleXmlDom.php on line <i>14</i>
+</th></tr>
+<tr><th align="left" bgcolor="#f57900" colspan="5">
+<span style="background-color: #cc0000; color: #fce94f; font-size: x-large;">( ! )</span> Error: Class "voku\helper\AbstractSimpleXmlDom" not found in C:\wamp64\www\Velotech\wp-content\plugins\simply-static\vendor\voku\simple_html_dom\src\voku\helper\SimpleXmlDom.php on line <i>14</i>
+</th></tr>
+<tr><th align="left" bgcolor="#e9b96e" colspan="5">Call Stack</th></tr>
+<tr>
+<th align="center" bgcolor="#eeeeec">#</th>
+<th align="left" bgcolor="#eeeeec">Time</th>
+<th align="left" bgcolor="#eeeeec">Memory</th>
+<th align="left" bgcolor="#eeeeec">Function</th>
+<th align="left" bgcolor="#eeeeec">Location</th>
+</tr>
+<tr>
+<td bgcolor="#eeeeec" align="center">1</td>
+<td bgcolor="#eeeeec" align="center">0.0018</td>
+<td bgcolor="#eeeeec" align="right">363768</td>
+<td bgcolor="#eeeeec">{main}(  )</td>
+<td title="C:\wamp64\www\Velotech\wp-content\plugins\simply-static\vendor\voku\simple_html_dom\src\voku\helper\SimpleXmlDom.php" bgcolor="#eeeeec">...\SimpleXmlDom.php<b>:</b>0</td>
+</tr>
+</table></font>
---
+++ wp-content/uploads/simply-static/temp-files/simply-static-1-1700131986/wp-content/plugins/simply-static/vendor/a5hleyrich/wp-background-processing/tests/bootstrap.php
@@ -0,0 +1,1 @@
+Could not find C:\WINDOWS\TEMP/wordpress-tests-lib/includes/functions.php, have you run bin/install-wp-tests.sh ?
